49b26c9cc00ed24a5ddb5da0199a5f0604179912
Antosphere <ironslinger256@gmail.com>
2018-08-11 14:43:56 +0800

    kill yourself

---
+++ CODE.py
@@ -0,0 +1,81 @@
+#PYTHON3#
+#Gender
+
+print("PLEASE ONLY CHOOSE/TYPE THE OPTIONS THAT ARE DISPLAYED, IF NOT U MAY RUN INTO PROBLEMS IF U DO RESTART THE APPLICATION")
+print("NOTE THIS HAS BEEN ONLY TESTED ON WINDOWS")
+print("USE PYTHON 3 and above")
+co=input("Press Enter To Continue")
+if co==co:
+    co=1
+import os
+import time
+os.system('cls||clear')
+print("Before we start, we'll need some details ")
+oco=input("Press Enter To Continue")
+if oco==oco:
+    oco=1
+os.system('cls||clear')
+print("What's your gender? Male or Female?[M/F]")
+gender=input("INPUT>>")
+
+
+if gender==("Male"):
+  print("Okay") 
+if gender==("Female"):
+  print("Okay")
+if gender==("M"):
+  gender=("Male")
+  print("Okay")
+if gender==("F"):
+  gender=("Female")
+  print("Okay")
+
+  
+
+  
+#Pronouns
+if gender==("Male"):
+  pronoun=("his")
+  pom=("man")
+if gender==("Female"):
+  pronoun=("her")
+  pom=("woman")
+
+print("Enter your name, adventurer:")
+name=input("INPUT>>")
+if name==name:
+  print("Ah, so your name is "+name)
+print("Press Enter to Start")
+start=input("INPUT>>")
+if start==start:
+      print("Along time ago...")
+      time.sleep(1)
+      print("In a galaxy far far away...")
+      time.sleep(1)
+      print("lived a "+pom+" who wanted to fight as many people as possible...")
+      time.sleep(1)
+      print(pronoun+" name was "+name)
+      time.sleep(1)
+oc=input("Press Enter To Continue")
+if oc==oc:
+    os.system('cls||clear')
+    print("There was life on Mars,but then, something happened...")
+    time.sleep(1)
+    print("What ever happened, it made Mars inhabitable.")
+    time.sleep(1)
+    print("At that time, Earth didnt have any life forms.")
+    print("Thus,the inhabitants of Mars came and 'pollinated' Earth,giving it life.")
+    time.sleep(1)
+    print("They eventually evolved into Humans,while the inhabitants of Mars hide away somewhere on Earth")
+    
+
+
+      
+
+    
+            
+            
+      
+        
+
+    
