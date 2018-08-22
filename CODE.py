20f80274ca9008d23853f0ee7b091eb31c74415f
ultraflame4 <34125174+ultraflame4@users.noreply.github.com>
2018-08-22 13:21:57 +0800

    Add files via upload

--- CODE.py
+++ CODE.py
@@ -1,15 +1,50 @@
 #PYTHON3#
+#Loads
+lai=0
+lott=1
+loadloop=102
+if lott==1:
+        dott=(".")
+info=0
+
+import os
+import time
+slll=1
+#Loads
+
+if slll==1:
+    loadloop=0
+while loadloop<100:
+    print("Note: Please Submit A Report To https://bit.ly/2w7v86R For Any Bugs Or Issues")
+    print("------------------------------------------------------------------------------------------")
+    print("Loading Resources"+dott)
+    print(lai)
+    lai+=1
+    lott+=1
+    loadloop+=1
+    if lai==101:
+        lai=0
+    if lott==4:
+        lott=1
+    if lott==3:
+        dott=("...")
+    if lott==2:
+        dott=("..")
+    if lott==1:
+        dott=(".")
+    time.sleep(0.1)
+    os.system('cls||clear')
+      
 #Gender
+os.system('cls||clear')
 print("Note: Please Submit A Report To https://bit.ly/2w7v86R For Any Bugs Or Issues")
 print("------------------------------------------------------------------------------------------")
-print("PLEASE ONLY CHOOSE/TYPE THE OPTIONS THAT ARE DISPLAYED, IF NOT U MAY RUN INTO PROBLEMS IF U DO RESTART THE APPLICATION")
-print("NOTE THIS HAS BEEN ONLY TESTED ON WINDOWS")
+print("PLEASE ONLY CHOOSE/TYPE THE OPTIONS THAT ARE DISPLAYED, IF NOT U MAY RUN INTO PROBLEMS ,IF U DO RESTART THE APPLICATION FIRST BEFORE SUBMITTING A REPORT")
+print("NOTE THIS HAS BEEN ONLY TESTED ON WINDOWS & MAC")
 print("USE PYTHON 3 and above")
 co=input("Press Enter To Continue")
 if co==co:
     co=1
-import os
-import time
 os.system('cls||clear')
 print("Note: Please Submit A Report To https://bit.ly/2w7v86R For Any Bugs Or Issues")
 print("------------------------------------------------------------------------------------------")
@@ -17,72 +52,79 @@ print("Before we start, we'll need some details ")
 oco=input("Press Enter To Continue")
 if oco==oco:
     oco=1
+while info<1:
+    info=1
+    os.system('cls||clear')
+    print("Note: Please Submit A Report To https://bit.ly/2w7v86R For Any Bugs Or Issues")
+    print("------------------------------------------------------------------------------------------")
+    print("What's your gender? Male or Female?[M/F]")
+    gender=input("INPUT>>")
 
-os.system('cls||clear')
-print("Note: Please Submit A Report To https://bit.ly/2w7v86R For Any Bugs Or Issues")
-print("------------------------------------------------------------------------------------------")
-print("What's your gender? Male or Female?[M/F]")
-gender=input("INPUT>>")
 
+    if gender==("Male"):
+        print("Okay") 
+    if gender==("Female"):       
+        print("Okay")
+    if gender==("M"):
+        gender=("Male")
+        print("Okay")
+    if gender==("F"):
+        gender=("Female")
+        print("Okay")
 
-if gender==("Male"):
-  print("Okay") 
-if gender==("Female"):
-  print("Okay")
-if gender==("M"):
-  gender=("Male")
-  print("Okay")
-if gender==("F"):
-  gender=("Female")
-  print("Okay")
-else:
-    print("TEST")
-
-  
 
   
-#Pronouns
-if gender==("Male"):
-  pronoun=("his")
-  pom=("man")
-if gender==("Female"):
-  pronoun=("her")
-  pom=("woman")
 
-#NAME#
-os.system('cls||clear')
-print("Note: Please Submit A Report To https://bit.ly/2w7v86R For Any Bugs Or Issues")
-print("------------------------------------------------------------------------------------------")
-print("Enter your name:")
-name=input("INPUT>>")
-if name==name:
-  print("Ok")
+  
+    #Pronouns
+    if gender==("Male"):
+      pronoun=("his")
+      pom=("man")
+    if gender==("Female"):
+      pronoun=("her")
+      pom=("woman")
 
-#AGE#
+    #NAME#
+    os.system('cls||clear')
+    print("Note: Please Submit A Report To https://bit.ly/2w7v86R For Any Bugs Or Issues")
+    print("------------------------------------------------------------------------------------------")
+    print("Enter your name:")
+    name=input("INPUT>>")
+    if name==name:
+      print("Ok")
+    
+    #AGE#
 
-os.system('cls||clear')
-print("Note: Please Submit A Report To https://bit.ly/2w7v86R For Any Bugs Or Issues")
-print("------------------------------------------------------------------------------------------")
+    os.system('cls||clear')
+    print("Note: Please Submit A Report To https://bit.ly/2w7v86R For Any Bugs Or Issues")
+    print("------------------------------------------------------------------------------------------")
 
-print("Enter your Age:")
-age=input("INPUT>>")
-if age==age:
-  print("Ok")
+    print("Enter your Age:")
+    age=input("INPUT>>")
+    if age==age:
+      print("Ok")
 
-#Checks Info#
-os.system('cls||clear')
-print("Note: Please Submit A Report To https://bit.ly/2w7v86R For Any Bugs Or Issues")
-print("------------------------------------------------------------------------------------------")
-print("Please Check That The Information Below Is What You Want")
-print("Name:"+name)
-time.sleep(0.5)
-print("You Are A "+gender)
-time.sleep(0.5)
-print("Age: "+age)
-time.sleep(10)
-      
-      
-      
+
+    #Checks Info#
+    os.system('cls||clear')
+    print("Note: Please Submit A Report To https://bit.ly/2w7v86R For Any Bugs Or Issues")
+    print("------------------------------------------------------------------------------------------")
+    print("Please Check That The Information Below Is What You Want")
+    print("Name:"+name)
+    time.sleep(0.5)
+    print("You Are A "+gender)
+    time.sleep(0.5)
+    print("Age: "+age)
+    time.sleep(0.5)
+    print("Type Back Or back or B to retype your Information,Press Enter To Continue")
+    cbo=input("INPUT>>")
+    if cbo==("B"):
+        info=0
+    if cbo==("Back"):
+        info=0
+    if cbo==("back"):
+        info=0
+    
       
 #STARTS#
 os.system('cls||clear')
